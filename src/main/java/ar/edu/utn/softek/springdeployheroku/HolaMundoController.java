debefc147a2650901879a2f363dee8b798202b35
Ezequiel Sosa <ezequieljsosa@gmail.com>
2022-01-26 15:02:32 -0300

    CORS config

--- src/main/java/ar/edu/utn/softek/springdeployheroku/HolaMundoController.java
+++ src/main/java/ar/edu/utn/softek/springdeployheroku/HolaMundoController.java
@@ -1,12 +1,14 @@
 package ar.edu.utn.softek.springdeployheroku;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "${FRONTORIGIN:http://localhost:3000}")
 public class HolaMundoController {
 
     @Autowired
