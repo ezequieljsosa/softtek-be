33661e848135ba4da01e38e96207c454231b5800
Ezequiel Sosa <ezequieljsosa@gmail.com>
2022-01-26 13:22:37 -0300

    otro commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# softtek-be
---
+++ src/main/java/ar/edu/utn/softek/springdeployheroku/HolaMundoController.java
@@ -0,0 +1,25 @@
+package ar.edu.utn.softek.springdeployheroku;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HolaMundoController {
+
+    @Autowired
+    AlgoRepo repo;
+
+    @GetMapping("/hola")
+    public String hola(){
+        return "hola";
+    }
+
+    @GetMapping("/algo")
+    public List<Algo> algo(){
+        return repo.findAll();
+    }
+
+}
